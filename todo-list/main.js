0676c866f5e4795192f3518739e7427b0ccecefb
JiyoungHyun <shyunji023@gmail.com>
2021-11-03 23:51:16 +0900

    Feat: todolist 기능

--- todo-list/main.js
+++ todo-list/main.js
@@ -1,63 +1,62 @@
 "use strict";
 
-const inputs = document.querySelectorAll("input");
-const plusBtns = document.querySelectorAll(".plusBtn");
-const items = document.querySelectorAll(".todo__items");
-const nav = document.querySelector("nav");
-const navBtn = document.querySelectorAll("nav > button");
-const todos = document.querySelectorAll(".todo");
+const todosItems = document.querySelectorAll(".todos__items");
+const todosInput = document.querySelectorAll(".todos__input");
+const addBtn = document.querySelectorAll(".todos__btn");
 
-nav.addEventListener("click", (e) => {
-  if (e.target.tagName !== "BUTTON") return;
-
-  switch (e.target.className) {
-    case "nav__today":
-      todos[0].classList.add("open");
-      todos[1].classList.remove("open");
-      break;
-    case "nav__tomorrow":
-      todos[0].classList.remove("open");
-      todos[1].classList.add("open");
-      break;
-    case "nav__together":
-      todos[0].classList.add("open");
-      todos[1].classList.add("open");
-      break;
-  }
-});
-
-plusBtns.forEach((btn, index) =>
-  btn.addEventListener("click", () => onAdd(index))
+// 리스트 추가
+addBtn.forEach((btn, index) =>
+  btn.addEventListener("click", () => {
+    onAdd(index);
+  })
 );
 
-inputs.forEach((input, index) =>
-  input.addEventListener("keyup", (e) => {
-    if (e.key == "Enter") {
+// 엔터 눌렀을 때 추가
+todosInput.forEach((input, index) =>
+  input.addEventListener("keyup", (event) => {
+    if (event.key === "Enter") {
       onAdd(index);
     }
   })
 );
-
-function createItem(text, index) {
+const onAdd = (index) => {
+  // 아무것도 안치고 그냥 엔터 눌렀을 때 리스트 올라가는 것 방지
+  if (!todosInput[index].value) return;
   const li = document.createElement("li");
   const span = document.createElement("span");
-  span.innerText = text;
-  const icon = document.createElement("i");
-  icon.setAttribute("class", "fas fa-trash-alt trashBtn");
+  const deleteBtn = document.createElement("button");
+
+  // class 이름 설정하는 과정
+  li.setAttribute("class", "todos__item");
+  span.setAttribute("class", "todos__name");
+  deleteBtn.setAttribute("class", "todos__delete");
+
+  deleteBtn.setAttribute("src", "/assets/icons/delete_icon.svg");
+  span.innerText = todosInput[index].value;
+
   li.appendChild(span);
-  li.appendChild(icon);
+  li.appendChild(deleteBtn);
+  todosItems[index].appendChild(li);
 
-  icon.addEventListener("click", () => {
-    items[index].removeChild(li);
+  todosInput[index].value = "";
+
+  deleteBtn.addEventListener("click", () => {
+    li.remove();
   });
-  return li;
-}
+};
 
-function onAdd(index) {
-  const text = inputs[index].value;
-  if (!text) return;
-  const item = createItem(text, index);
-  items[index].appendChild(item);
-  inputs[index].value = "";
-  inputs[index].focus();
-}
+const nav = document.querySelector(".options");
+const todos = document.querySelectorAll(".todos > section");
+console.log(todos);
+nav.addEventListener("click", (event) => {
+  if (event.target.className.includes("options__today")) {
+    todos[0].classList.add("open");
+    todos[1].classList.remove("open");
+  } else if (event.target.className.includes("options__tomorrow")) {
+    todos[0].classList.remove("open");
+    todos[1].classList.add("open");
+  } else if (event.target.className.includes("options__both")) {
+    todos[0].classList.add("open");
+    todos[1].classList.add("open");
+  }
+});
