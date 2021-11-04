8b7ed0414e0c35f9451752d530fb8f9538ce0850
JiyoungHyun <shyunji023@gmail.com>
2021-11-04 12:27:05 +0900

    Feat:  modal 구현

--- velog/main.js
+++ velog/main.js
@@ -1,5 +1,6 @@
 "use strict";
 
+// dropdown
 const period = document.querySelector(".period");
 const dropdown = document.querySelector(".dropdown");
 
@@ -18,4 +19,36 @@ dropdown.addEventListener("click", (e) => {
     element.classList.remove("active");
   });
   e.target.classList.add("active");
+});
+
+// modal
+const cardContainer = document.querySelector(".cards");
+const card = document.querySelectorAll(".card");
+
+cardContainer.addEventListener("click", (e) => {
+  let el = e.target;
+
+  while (el && !el.classList.contains("card")) {
+    el = el.parentNode;
+  }
+  if (el.classList.contains("card__modal")) return;
+  console.log(el);
+  const modalBackground = document.createElement("div");
+  const modalCard = document.createElement("article");
+  modalBackground.appendChild(modalCard);
+  modalBackground.setAttribute("class", "card__modal-bg");
+  cardContainer.appendChild(modalBackground);
+  modalCard.innerHTML = el.innerHTML;
+  modalCard.setAttribute("class", "card card__modal");
+
+  const modalCloseBtn = document.createElement("button");
+  modalCloseBtn.innerText = "X";
+  modalCloseBtn.setAttribute("class", "card__modal-close-btn");
+  modalCard.appendChild(modalCloseBtn);
+  document.body.style.overflow = "hidden";
+
+  modalCloseBtn.addEventListener("click", () => {
+    modalBackground.remove();
+    document.body.style.overflow = "visible";
+  });
 });
