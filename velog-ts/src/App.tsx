6006726e0aa820ce01a8bf4096c27eaf2406b88b
JiyoungHyun <shyunji023@gmail.com>
2021-12-18 02:02:31 +0900

    Feat: routing

--- velog-ts/src/App.tsx
+++ velog-ts/src/App.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/home";
+import Write from "./pages/write";
 
 function App() {
-  return <div>하이하이</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/*" element={<Home />}></Route>
+        <Route path="/write" element={<Write />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
