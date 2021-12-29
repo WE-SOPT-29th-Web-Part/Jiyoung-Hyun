ee6d0ad61ec7d1e950f37904cdb3ec73f842bceb
JiyoungHyun <shyunji023@gmail.com>
2021-12-30 00:11:27 +0900

    Feat: history 기능 구현

--- githubfinder-ts/src/components/SearchBar.tsx
+++ githubfinder-ts/src/components/SearchBar.tsx
@@ -1,16 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { IUserInfo } from "../types/user.type";
 import { client } from "../libs/api";
+import SearchHistory from "./SearchHistory";
 interface IProps {
   setUserInfo: React.Dispatch<React.SetStateAction<IUserInfo>>;
 }
 function SearchBar({ setUserInfo }: IProps): React.ReactElement {
   const [user, setUser] = useState("");
+  const [historyList, setHistoryList] = useState<string[]>([]);
+
+  useEffect(() => {
+    console.log(localStorage);
+    const storedHistory = localStorage.getItem("history");
+    if (storedHistory) setHistoryList(JSON.parse(storedHistory));
+  }, []);
+
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setUser(e.target.value);
   };
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+
+  const handleUserInfo = async (user: string) => {
     setUserInfo((currentUserInfo) => ({
       ...currentUserInfo,
       status: "pending",
@@ -30,15 +39,32 @@ function SearchBar({ setUserInfo }: IProps): React.ReactElement {
       }));
     }
   };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    handleUserInfo(user);
+
+    const newHistory = Array.from(new Set([...historyList, user])).slice(-3);
+    setHistoryList(newHistory);
+    localStorage.setItem("history", JSON.stringify(newHistory));
+    setUser("");
+  };
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        value={user}
-        onChange={handleChange}
-        placeholder="Github ID"
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={user}
+          onChange={handleChange}
+          placeholder="Github ID"
+        />
+      </form>
+      <SearchHistory
+        historyList={historyList}
+        setHistoryList={setHistoryList}
+        handleUserInfo={handleUserInfo}
       />
-    </form>
+    </>
   );
 }
 
