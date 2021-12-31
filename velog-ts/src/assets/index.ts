33e7eba1ed6daaec0fd0bcc43151d6e3ed0ebb52
JiyoungHyun <shyunji023@gmail.com>
2021-12-31 15:12:20 +0900

    Etc: 에셋추가

---
+++ velog-ts/src/assets/index.ts
@@ -0,0 +1,7 @@
+export { default as arrowDropDownIcon } from "./arrowDropDownIcon.svg";
+export { default as arrowLeftIcon } from "./arrowLeftIcon.svg";
+export { default as githubIcon } from "./githubIcon.svg";
+export { default as homeIcon } from "./homeIcon.svg";
+export { default as mailIcon } from "./mailIcon.png";
+export { default as searchIcon } from "./searchIcon.svg";
+export { default as profileImg } from "./sopt_web.png";
